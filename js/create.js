cf4df22cc99b2c06c9a03389a6c5b87878522229
Anudit Nagar <nagaranudit@gmail.com>
2020-09-12 12:07:40 +0530

    Cleanups

--- js/create.js
+++ js/create.js
@@ -14,19 +14,41 @@ function create(e){
     if(e){
         e.preventDefault();
     }
-    let createBtn = document.querySelector('#createBtn');
-    createBtn.innerText = "Aww Yuss";
-    createBtn.classList.add('disabled');
-    createDataToken(
-        document.querySelector('#inp_name').value,
-        document.querySelector('#inp_sym').value,
-        document.querySelector('#inp_cap').value,
-        document.querySelector('#inp_blob').value,
-    ).then(()=>{
-        createBtn.innerText = "Txn is being Mined...";
-    }).catch((e)=>{
-        alert(e.message);
-        createBtn.classList.remove('disabled');
-        createBtn.innerText = "Create";
-    });
+
+    if(document.querySelector('#inp_name').value === '' ||
+    document.querySelector('#inp_sym').value === '' ||
+    document.querySelector('#inp_cap').value === '' ||
+    document.querySelector('#inp_blob').value === ''
+    ){
+        Swal.fire({
+            icon: 'warning',
+            title: 'Incomplete Submission',
+            html: `Some values seem to be empty`,
+        });
+    }
+    else{
+        let createBtn = document.querySelector('#createBtn');
+        createBtn.innerText = "Aww Yuss";
+        createBtn.classList.add('disabled');
+        createDataToken(
+            document.querySelector('#inp_name').value,
+            document.querySelector('#inp_sym').value,
+            document.querySelector('#inp_cap').value,
+            document.querySelector('#inp_blob').value,
+        ).then(()=>{
+            createBtn.innerText = "Txn is being Mined...";
+        }).catch((e)=>{
+            alert(e.message);
+            createBtn.classList.remove('disabled');
+            createBtn.innerText = "Create";
+        });
+    }
+
+}
+
+function clearInput(){
+    document.querySelector('#inp_name').value = '';
+    document.querySelector('#inp_sym').value = '';
+    document.querySelector('#inp_cap').value = '';
+    document.querySelector('#inp_blob').value = '';
 }
